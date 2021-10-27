55fbb223dd8cd446384b4da93d7166eaa564d3e0
SchleicherConfalonieriN <76877741+SchleicherConfalonieriN@users.noreply.github.com>
2021-10-26 23:02:53 -0300

    Add files via upload

---
+++ controllers/SacarTurnoConMedico.php
@@ -0,0 +1,58 @@
+<?php
+
+//controlador
+
+require '../fw/fw.php';
+require './Sesion.php';
+require '../models/Medico.php';
+require '../models/Turno.php';
+require '../views/SacarTurnoConMedico.php';
+
+
+
+if(!isset($_GET['id'])) {} 
+else{
+	header('Location:./IngresoAlSistema.php');
+	exit();
+}
+
+if(!ctype_digit($_GET['id']){
+	header('Location:./IngresoAlSistema.php');
+	exit();
+}
+
+$dni_medico=$_GET['id'];
+
+$m=new Medico();
+$t=new Turno();
+$TurnosDisponibles=[];
+
+if(isset($_GET['dia'])){
+	//vlidar fecha
+	$fecha=$_GET('dia');
+	
+	$turnosPosibles=$m->generarHorariosDeAtencion();
+	
+	$turnosAgendados=$t->getTurnosAgendados($dni_medico,$fecha);
+	
+	foreach($turnosPosibles as $tp){
+		$libre=true;
+		foreach($turnosAgendados as $ta){
+			if($tp==$ta['hora']) $libre=false;
+		}
+		if ($libre=true) array_push($turnosDisponibles,$tp);
+	}
+	
+}
+
+$v=new SacarTurnoConMedico();
+$v->opciones=$TurnosDisponibles=[];
+$v->render();
+
+
+
+
+
+
+
+
