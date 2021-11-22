a3f3240d6995ab929d4070667e5ee2cafcc77a18
brunozicari <81536471+brunozicari@users.noreply.github.com>
2021-11-22 10:57:46 -0300

    Add files via upload

--- html/AgregarEstudio.php
+++ html/AgregarEstudio.php
@@ -7,12 +7,9 @@
 <head>
 	<title>Estudio</title>
 	<link rel="stylesheet" type="text/css" href="../css/fondo.css">
-	<link rel="stylesheet" type="text/css" href="../css/admin.css">
 </head>
 <body>
-	
 <h2>Nuevo estudio:</h2>
-<div class="registro">
 <form id="formulario" method="post">
 	<label for="nombre">Nombre:</label><br>
 	<input type="text" id="nombre" name="nombre" required="required"><br>
@@ -28,8 +25,6 @@
   <input type="submit" value="Enviar"></input>
 </form> 
 <br></br>
-</div>
-<h2>Listado de Estudios</h2>
 <table>
 	<tr><th>Nombre</th><th>Descripción</th><th>Precio</th><th>Horario</th></tr>
 
@@ -38,9 +33,9 @@
 	<?php } ?>
 </table>
 
-<div>
+
 <br/>
 <a href="./MenuPrincipalAdministracion.php"><button>Volver</button></a>
-</div>
+
 </body>
 </html>
--- html/NuevoMedico.php
+++ html/NuevoMedico.php
@@ -7,11 +7,9 @@
 <head>
 	<title>Nuevo Medico</title>
 	<link rel="stylesheet" type="text/css" href="../css/fondo.css">
-	<link rel="stylesheet" type="text/css" href="../css/admin.css">
 </head>
 <body>
-<h2>Registrar Profesional</h2>
-<div>
+<h2>Datos del profesional</h2>
 <form id="formulario" method="post">
 	<label for="dni">DNI:</label><br>
 	<input type="number" id="dni" name="dni" required="required"><br>
@@ -25,26 +23,25 @@
 	<input type="text" id="mail" name="mail" required="required"><br><br>
 	<label for="especialidad">Especialidad:</label><br>
 	<select name="especialidad" id="especialidad" required="required">
-		<?php foreach($this->especialidades as $e){ ?><br>
-		<option value="<?=htmlentities($e['especialidad_id']) ?>"><?=htmlentities($e['nom_especialidad']) ?></option><?php } ?>
-	</select> <br><br>
-	<label for="consultorio">Consultorio:</label><br>	
+		<?php foreach($this->especialidades as $e){ ?>
+		<option value="<?=$e['especialidad_id'] ?>"><?=$e['nom_especialidad'] ?></option><?php } ?>
+	</select> <br>
+	<label for="consultorio">Consultorio:</label><br>
 	<select name="consultorio" id="consultorio" required="required">
 		<?php foreach($this->consultorios as $c){ ?>
-		<option value="<?=htmlentities($c) ?>"><?=htmlentities($c) ?></option><?php } ?>
-	</select><br><br>
+		<option value="<?=$c ?>"><?=$c ?></option><?php } ?>
+	</select><br>
 	<label for="horario">Horario:</label><br>
 	<select name="horario" id="horario" required="required">
 		<option value="m">Mañana</option>
 		<option value="t">Tarde</option>
-	</select> <br><br>
+	</select> <br>
   <input type="submit" value="Enviar"></input>
 </form> 
-
 <h3><?= $this->mensaje ?></h3>
 <br/>
 <a href="./MenuPrincipalAdministracion.php"><button>Volver</button></a>
-</div>
+
 </body>
 </html>
 
