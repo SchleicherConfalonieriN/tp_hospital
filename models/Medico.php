85d124a68cb2f95c6eddacd0564351b8e7e1773c
brunozicari <81536471+brunozicari@users.noreply.github.com>
2021-11-22 12:22:32 -0300

    Update Medico.php

--- models/Medico.php
+++ models/Medico.php
@@ -7,8 +7,7 @@ require_once '../models/Usuario.php';
 
 class Medico extends Usuario {
 	
-	public function getTodosPorEspecialidad($id){
-		
+	public function getTodosPorEspecialidad($id){		
 		seguridad::id_validacion($id);
 		$this->db->query("SELECT * FROM medicos LEFT JOIN especialidades ON medicos.especialidad=especialidades.especialidad_id where especialidad = " . $id);
 		return $this->db->fetchAll();
@@ -19,23 +18,20 @@ class Medico extends Usuario {
 		return $this->db->fetchAll();
 	}
 	
-	public function informacion($dni){//veer
-		
+	public function informacion($dni){//veer		
 		seguridad::dni_validacion($dni);
 		$this->db->query("SELECT * from medicos where dni='$dni'");
 		return $this->db->fetchAll();
 	}
 	
-	public function existeMedico($dni){
-		
+	public function existeMedico($dni){		
 		seguridad::dni_validacion($dni);
 		$this->db->query("SELECT * FROM medicos WHERE dni = '$dni'");
 		if (($this->db->numRows()==1)) return true; //si lo encuentra entonces existe
 		return false;
 	}
 	
-	public function generarHorariosDeAtencion($dni){
-		
+	public function generarHorariosDeAtencion($dni){		
 		seguridad::dni_validacion($dni);
 		$this->db->query("SELECT horario FROM medicos where dni = " . $dni . " limit 1");
 		if (($this->db->numRows()==0)) throw new ValidationException("Dni no corresponde a ningún medico");
@@ -50,8 +46,7 @@ class Medico extends Usuario {
 		return $retorno;
 	}
 	
-	public function verificarHora($dni_medico,$hora_a_verificar){
-		
+	public function verificarHora($dni_medico,$hora_a_verificar){		
 		seguridad::dni_validacion($dni_medico);
 		$this->db->query("SELECT horario FROM medicos where dni = " . $dni_medico . " limit 1");
 		if (($this->db->numRows()==0)) throw new ValidationException("Dni no corresponde a ningún medico");
@@ -65,8 +60,7 @@ class Medico extends Usuario {
 		return false;
 	}
 	
-	public function darDeAltaMedico($dni,$nombre,$apellido,$especialidad,$horario,$consultorio){
-		
+	public function darDeAltaMedico($dni,$nombre,$apellido,$especialidad,$horario,$consultorio){		
 		seguridad::dni_validacion($dni);
 		seguridad::nombre_validacion($nombre);
 		seguridad::nombre_validacion($apellido);
@@ -78,23 +72,22 @@ class Medico extends Usuario {
 		$this->db->query("INSERT INTO medicos (dni, nom_medico, ape_medico, especialidad, horario, consultorio) VALUES ('$dni', '$nombre', '$apellido', '$especialidad', '$horario', '$consultorio')");
 	}
 	
-	public function cambiarConsultorio($dni,$consultorio){
-		
+	public function cambiarConsultorio($dni,$consultorio){		
 		seguridad::dni_validacion($dni);
 		seguridad::consultorio_validacion($consultorio);
 		$this->db->query("UPDATE medicos SET consultorio = '$consultorio' where dni='$dni'");
 	}
 	
-	public function getConsultorio($dni){
-		
+	public function getConsultorio($dni){		
 		seguridad::dni_validacion($dni);
 		$this->db->query("SELECT consultorio FROM medicos WHERE dni = '$dni' limit 1");
 		if (($this->db->numRows()==0)) throw new ValidationException("Dni invalido");
 		$t = $this->db->fetch();
 		return $t['consultorio'];
 	}
+	
 	public function eliminarMedico($dni){
-	 seguridad::dni_validacion($dni);
+		seguridad::dni_validacion($dni);
 		$this->db->query("delete from usuarios where dni='$dni'");
 		$this->db->query("delete from medicos where dni='$dni'");
 		$this->db->query("delete from turnos where servicio='$dni'");
@@ -103,8 +96,7 @@ class Medico extends Usuario {
 
 	public function cambiar_horario ($dni,$turno){
 		seguridad::dni_validacion($dni);
-		seguridad::horario_validacion($turno);
-			
+		seguridad::horario_validacion($turno);			
 		$this->db->query("UPDATE `medicos` SET horario = '$turno' 
 		where dni='$dni'");
 	}
