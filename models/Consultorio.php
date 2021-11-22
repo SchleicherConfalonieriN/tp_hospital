7de0a684597e8ae091732aab9541b08722c84fea
brunozicari <81536471+brunozicari@users.noreply.github.com>
2021-11-22 12:20:10 -0300

    Update Consultorio.php

--- models/Consultorio.php
+++ models/Consultorio.php
@@ -10,26 +10,25 @@ class Consultorio extends Model {
 		return $this->db->fetchAll();
 	}
 	
-	public function esConsultorioLibre($numero){
-	
+	public function esConsultorioLibre($numero){	
 		seguridad::consultorio_validacion($numero);
 		$this->db->query("SELECT * FROM medicos where consultorio = '$numero'");
 		if (($this->db->numRows()==0)) return true;
 		return false;
 	}
 	
-	public function existeConsultorio($numero){
-	
+	public function existeConsultorio($numero){	
 		seguridad::consultorio_validacion($numero);
 		$this->db->query("SELECT * FROM consultorios where numero = '$numero'");
 		if (($this->db->numRows()==0)) return false;
 		return true;
 	}
 
-		public function eliminarConsultorio($id){
-	 seguridad::id_validacion($id);
-	$this->db->query("delete from consultorios where consultorio_id='$id'");
-	}	
+	public function eliminarConsultorio($id){
+		seguridad::id_validacion($id);
+		$this->db->query("delete from consultorios where consultorio_id='$id'");
+	}
+	
 	public function darDeAlta($consultorio){
 		seguridad::consultorio_validacion($consultorio);
 		$this->db->query("INSERT INTO consultorios (numero) VALUES ('$consultorio')");
